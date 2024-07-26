eb1f83dc1e043d7505d21b8b6f0e6d61b7f30dcf
Trent Schafer <trent.schafer@gmail.com>
2024-07-27 09:21:48 +1000

    Handle new document markup

--- source/guardian/guardian.go
+++ source/guardian/guardian.go
@@ -20,15 +20,21 @@ import (
 type GuardianSource struct{}
 
 const sourceName string = "guardian"
+const serverUrl string = "https://www.theguardian.com"
 
 func (s GuardianSource) GetPhoto(applyWallpaper bool) {
 
 	imageIndexresp := httpclient.GetHttpResponse("https://www.theguardian.com/news/series/ten-best-photographs-of-the-day")
 	picOfDayDoc, _ := goquery.NewDocumentFromReader(imageIndexresp.Body)
 
-	picDivs := picOfDayDoc.Find("div.fc-container__inner a.u-faux-block-link__overlay")
+	picDivs := picOfDayDoc.Find("a[data-link-name*=\"media\"][data-link-name*=\"group-0\"]")
 	todaysPicPage, _ := picDivs.First().Attr("href")
 
+	if len(todaysPicPage) == 0 {
+		log.Fatal("Couldn't find pic of the day card. Check selector is still correct.")
+	}
+
+	todaysPicPageResp := httpclient.GetHttpResponse(fmt.Sprintf("%s%s", serverUrl, todaysPicPage))
 	picOfDayDoc, _ = goquery.NewDocumentFromReader(todaysPicPageResp.Body)
 
 	sources := picOfDayDoc.Find("div.content picture source")
@@ -52,6 +58,7 @@ func (s GuardianSource) GetPhoto(applyWallpaper bool) {
 	readerWithProgress := progressBar.NewProxyReader(todaysPhotoResp.Body)
 	var savedFile *os.File
 	var destWallpaperfile string
+	var err error
 	sourceFilename := path.Base(imageURL.Path)
 	if applyWallpaper {
 		dataDir := config.GetDataDir()
